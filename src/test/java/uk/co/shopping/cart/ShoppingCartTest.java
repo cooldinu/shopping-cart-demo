c8c7d01877a9fe2632a8c6dd757f64ef939613b8
Dinesh H <denukumar@hotmail.com>
2017-02-02 21:32:37 +0000

    added unit test case with an empty setup and teardown methods

---
+++ src/test/java/uk/co/shopping/cart/ShoppingCartTest.java
@@ -0,0 +1,21 @@
+package uk.co.shopping.cart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by dinesh on 02/02/2017.
+ */
+public class ShoppingCartTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+}

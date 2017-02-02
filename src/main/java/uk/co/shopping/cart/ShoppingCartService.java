2b42a5872476eef131e3a528abb16d43a0503451
Dinesh H <denukumar@hotmail.com>
2017-02-02 22:03:45 +0000

    added multiple positive unit test cases which fail as the implementation in the service class is pending

--- src/main/java/uk/co/shopping/cart/ShoppingCartService.java
+++ src/main/java/uk/co/shopping/cart/ShoppingCartService.java
@@ -1,7 +1,13 @@
 package uk.co.shopping.cart;
 
+import java.util.List;
+
 /**
  * Created by dinesh on 02/02/2017.
  */
 public class ShoppingCartService {
+
+    public double checkout(List<Product> products) {
+        return 0d;
+    }
 }
--- src/test/java/uk/co/shopping/cart/ShoppingCartTest.java
+++ src/test/java/uk/co/shopping/cart/ShoppingCartTest.java
@@ -2,6 +2,12 @@ package uk.co.shopping.cart;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by dinesh on 02/02/2017.
@@ -18,6 +24,52 @@ public class ShoppingCartTest {
     @After
     public void tearDown() {
 
+    }
+
+    @Test
+    public void checkoutSingleApple() throws Exception {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "Apple", 0.6));
+
+        assertEquals("Shopping cart checkout price ", 0.6d, shoppingCartService.checkout(products), 0d);
+    }
+
+    @Test
+    public void checkoutThreeApples() throws Exception {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "Apple", 0.6));
+        products.add(new Product(1, "Apple", 0.6));
+        products.add(new Product(1, "Apple", 0.6));
+
+        assertEquals("Shopping cart checkout price ", 1.8d, shoppingCartService.checkout(products), 0d);
+    }
+
+    @Test
+    public void checkoutSingleOrange() throws Exception {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(2, "Orange", 0.25));
+
+        assertEquals("Shopping cart checkout price ", 0.25d, shoppingCartService.checkout(products), 0d);
+    }
+
+    @Test
+    public void checkoutThreeOranges() throws Exception {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(2, "Orange", 0.25));
+        products.add(new Product(2, "Orange", 0.25));
+
+        assertEquals("Shopping cart checkout price ", 0.5d, shoppingCartService.checkout(products), 0d);
+    }
+
+    @Test
+    public void checkoutMultipleItems() throws Exception {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "Apple", 0.6));
+        products.add(new Product(1, "Apple", 0.6));
+        products.add(new Product(2, "Orange", 0.25));
+        products.add(new Product(1, "Apple", 0.6));
+
+        assertEquals("Shopping cart checkout price ", 2.05d, shoppingCartService.checkout(products), 0d);
     }
 
 }
